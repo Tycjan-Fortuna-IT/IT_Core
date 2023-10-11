46319df4acc7c5e56cfc7382cf510f5ff9e31810
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2023-10-11 09:44:16 +0200

    GUIApplication template

--- src/Platform/GUI/Core/GUIApplication.cpp
+++ src/Platform/GUI/Core/GUIApplication.cpp
@@ -1,6 +1,26 @@
 #include "pch.hpp"
 #include "GUIApplication.hpp"
 
-namespace Core {
+#include "Engine/Core/Debug/Logger.hpp"
 
+namespace Core {
+    Application* Application::s_Instance = nullptr;
+
+    Application::Application(const ApplicationSpecification& specification)
+        : m_Specification(specification) {
+
+        ASSERT(!s_Instance, "Application already exists!");
+        CORE_TRACE("Creating application...");
+
+        s_Instance = this;
+
+    }
+
+    void Application::Run() {
+
+    }
+
+    void Application::Close() {
+
+    }
 }
--- src/Platform/GUI/Core/GUIApplication.hpp
+++ src/Platform/GUI/Core/GUIApplication.hpp
@@ -1,7 +1,57 @@
 #pragma once
 
-namespace Core {
-    class Application {
+#include <string>
 
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv);
+
+namespace Core {
+    enum class ApplicationType {
+        Window,
+        Console
     };
+
+    struct ApplicationCommandArguments final {
+        int Argc{ 0 };
+        char** Argv{ nullptr };
+    };
+
+    struct ApplicationSpecification final {
+        std::string Name{ "Core Application" };
+        ApplicationType Type{ ApplicationType::Window };
+        uint32_t Width{ 0 }; // If Type is Window, Width and Height are required
+        uint32_t Height{ 0 };
+        ApplicationCommandArguments CommandArguments; // If Type is Console, CommandArguments are required
+    };
+
+    class Application {
+    public:
+
+        Application(const ApplicationSpecification& specification);
+        virtual ~Application() = default;
+
+        Application(const Application& other) = delete;
+        Application(Application&& other) = delete;
+
+        const ApplicationSpecification& GetSpecification() { return m_Specification; }
+        bool IsRunning() const { return m_Running; }
+
+        static Application& Get() { return *s_Instance; }
+
+        void Close();
+
+    protected:
+
+        ApplicationSpecification m_Specification;
+        bool m_Running{ true };
+
+    private:
+
+        void Run();
+
+        static Application* s_Instance;
+
+        friend int ::main([[maybe_unused]] int argc, [[maybe_unused]] char** argv);
+    };
+
+    Application* CreateApplication([[maybe_unused]] int argc, [[maybe_unused]] char** argv);
 }

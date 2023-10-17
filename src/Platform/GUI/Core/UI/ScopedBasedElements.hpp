df43d56606175d8fc76ab6035178cf1074783a46
wuetenderzucker <104497910+wuetenderzucker@users.noreply.github.com>
2023-10-17 12:46:21 +0200

    Scope-based classes for color, font, style and ID

---
+++ src/Platform/GUI/Core/UI/ScopedBasedElements.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include "pch.hpp"
+
+#include "Platform/GUI/Core/UI/Color.hpp"
+
+namespace Core {
+
+    // Color
+    class ScopedColor {
+    public:
+
+        ScopedColor(ImGuiCol_ element, ImU32 color) {
+            ImGui::PushStyleColor(element, color);
+        };
+
+        ~ScopedColor() { ImGui::PopStyleColor(); };
+
+    };
+
+    // Font
+    class ScopedFont {
+    public:
+
+        ScopedFont(int index) {
+            ImGui::PushFont(ImGui::GetIO().Fonts->Fonts[index]);
+        };
+
+        ~ScopedFont() { ImGui::PopFont(); };
+
+    };
+
+    // Style
+    class ScopedStyle {
+    public:
+
+        template <typename T>
+        ScopedStyle(ImGuiStyleVar_ element, T value) {
+            ImGui::PushStyleVar(element, value);
+        };
+
+        ~ScopedStyle() { ImGui::PopStyleVar(); };
+    }; 
+
+    // ID
+    class ScopedID {
+    public:
+
+        ScopedID(int value) { ImGui::PushID(value); };
+
+        ~ScopedID() { ImGui::PopID(); };
+    };
+}
